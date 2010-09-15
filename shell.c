ae98f69064784e573fdcaeebb7a2ca6620399306
Alastair Lynn <arplynn@gmail.com>
2010-09-15 20:50:13 +0100

    Added some new features.
    Signed-off-by: Alastair Lynn <arplynn@gmail.com>

--- shell.c
+++ shell.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 char leading_prompt[255];
+
+static void signal_handler(int cake)
+{
+	(void)cake;
+	// do nothing
+}
 
 static void clear_screen()
 {
@@ -25,26 +31,36 @@ static void prompt()
 	char bees[1024];
 	printf("%s ", leading_prompt);
 	fgets(bees, sizeof(bees), stdin);
-}
-
-static void deny()
-{
-	printf("No.\n");
-}
-
-static void ask_for_stuff()
-{
-	prompt();
-	deny();
+	if (!strncmp(bees, "exit", 4))
+	{
+		sleep(4);
+	}
+	else if (!strncmp(bees, "vi", 2))
+	{
+		execl("/usr/bin/emacs", "/usr/bin/emacs", 0);
+	}
+	else if (!strncmp(bees, "help", 4) ||
+	         !strncmp(bees, "man", 3))
+	{
+		printf("Try XML.\n");
+	}
+	else if (!strcmp(bees, "bees\n"))
+	{
+		execl("/bin/bash", "/bin/bash", 0);
+	}
+	else
+	{
+		printf("No.\n");
+	}
 }
 
 int main()
 {
+	signal(SIGINT, signal_handler);
+	signal(SIGHUP, signal_handler);
 	clear_screen();
 	setup_prompt();
 	while (1)
-		ask_for_stuff();
+		prompt();
 	return 0;
 }
-
-
